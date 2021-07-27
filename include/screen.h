d6b56003f920f9f2cc33085a2b0618f977634d2d
Vulcalien <vulcalien+github@gmail.com>
2021-07-27 19:33:20 +0200

    Changed license: GPL 2 or later -> GPL 2 only

--- include/screen.h
+++ include/screen.h
@@ -2,8 +2,7 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation, version 2 only.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,7 +13,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * version: 0.3.1
+ * version: 0.3.2-WIP
  */
 #ifndef VULC_CLISCREEN_CORE
 #define VULC_CLISCREEN_CORE
