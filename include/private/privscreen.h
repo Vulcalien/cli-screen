9160f392121ad3d34b948ada819c32d622270d30
Vulcalien <vulcalien@gmail.com>
2021-12-17 00:43:58 +0100

    Change License to Lesser GPL 3 only

--- include/private/privscreen.h
+++ include/private/privscreen.h
@@ -1,17 +1,17 @@
 /* Copyright 2021 Vulcalien
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 2 only.
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, version 3 only.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ * If not, see <https://www.gnu.org/licenses/>.
  */
 #ifndef VULC_CLISCREEN_SCREEN_P
 #define VULC_CLISCREEN_SCREEN_P
